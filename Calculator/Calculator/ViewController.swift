3ef4f61b544782aa98388e8d0720df8d01e4f661
dkonayuki <dkonayuki@gmail.com>
2016-04-01 00:56:31 +0900

    Day 9: Calculator #2

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var display: UILabel!
     
-    var userIsInTheMiddleOfTypingANumber: Bool = false
+    var userIsInTheMiddleOfTypingANumber = false
     
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
@@ -21,6 +21,53 @@ class ViewController: UIViewController {
             display.text = digit
         } else {
             display.text = display.text! + digit
+        }
+    }    
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if (userIsInTheMiddleOfTypingANumber) {
+            enter()
+        }
+        switch operation {
+        case "×": performOperation { $0 * $1 }
+        case "+": performOperation { $0 + $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        default: break
+        }
+    }
+    
+    func performOperation(operation: (Double, Double) -> Double) {
+        if (operandStack.count >= 2) {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    private func performOperation(operation: Double -> Double) {
+        if (operandStack.count >= 1) {
+            displayValue = operation(operandStack.removeLast())
+            enter()
+        }
+    }
+    
+    var operandStack = [Double]()
+    @IBAction func enter() {
+        userIsInTheMiddleOfTypingANumber = false
+        operandStack.append(displayValue)
+        print("operandStack = \(operandStack)")
+    }
+    
+    //computed property
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+            userIsInTheMiddleOfTypingANumber = false
         }
     }
     
